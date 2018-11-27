05f300e53323587ae3ec74083b06dca968f8ea3d
patzux <ppradoexa@gmail.com>
2018-11-26 23:41:08 -0300

    added custom response messages

--- Week1/src/main/java/shopping/SwaggerConfig.java
+++ Week1/src/main/java/shopping/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static springfox.documentation.builders.PathSelectors.ant;
 import static springfox.documentation.builders.PathSelectors.regex;
@@ -26,20 +28,35 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+        final List<ResponseMessage> responseMessageList = new ArrayList<ResponseMessage>();
+        responseMessageList.add(new ResponseMessageBuilder()
+            .code(500)
+            .message("Server Error")
+            .responseModel(new ModelRef("Error"))
+            .build());
+            responseMessageList.add(new ResponseMessageBuilder()
+            .code(403)
+            .message("Forbidden!")
+            .build());
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+            .useDefaultResponseMessages(false)       
+            .globalResponseMessage(RequestMethod.GET, responseMessageList)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build(); 
+                                                     
+                
+            
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
-                "My REST API",
-                "Some custom description of API.",
+                "Shopping cart API",
+                "Allows creating of items and a shopping cart",
                 "API TOS",
                 "Terms of service",
-                new Contact("pato", "www.example.com", "myeaddress@company.com"),
+                new Contact("Patricio Prado", "www.example.com", "myeaddress@company.com"),
                 "License of API", "API license URL", Collections.emptyList());
     }
 
